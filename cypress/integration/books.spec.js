d3fbd19ae0a5278c02c7da6a3db62d9cb2126bcf
Zonghao Li <675375410@qq.com>
2018-12-10 18:49:54 +0000

    add more more time

--- cypress/integration/books.spec.js
+++ cypress/integration/books.spec.js
@@ -2,7 +2,7 @@ describe('Login and Book operations', () => {
   //need to login first and get the authority to enter books list
   it('should login first ', function () {
     cy.visit('http://localhost:8081/#/login')
-    cy.wait(10000)
+
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__inner')
       .type('qqq')
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__inner')
@@ -12,9 +12,13 @@ describe('Login and Book operations', () => {
   });
   //add a new book for deleting
   it('should add a book for deleting ', function () {
+    cy.wait(5000)
     cy.get(':nth-child(1) > .form__input').type('sun&beach')
+    cy.wait(5000)
     cy.get(':nth-child(2) > .form__input').type('asdad')
+    cy.wait(5000)
     cy.get(':nth-child(3) > .form__input').type('qwdhiasdihasd')
+    cy.wait(5000)
     cy.get(':nth-child(4) > .btn').click()
     cy.wait(3000)
   });
@@ -23,8 +27,11 @@ describe('Login and Book operations', () => {
     cy.visit('http://localhost:8081/#/books')
     cy.wait(10000)
     cy.get(':nth-child(10) > :nth-child(2)').should('contain','sun&beach')
+    cy.wait(5000)
     cy.get(':nth-child(10) > :nth-child(3)').should('contain','asdad')
+    cy.wait(5000)
     cy.get(':nth-child(10) > :nth-child(4)').should('contain','qwdhiasdihasd')
+    cy.wait(5000)
     cy.get(':nth-child(10) > :nth-child(6) > .fa').click()
     cy.wait(1500)
     cy.get('.swal2-confirm').click()
