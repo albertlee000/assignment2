d43903ce70657691867807e3b93f04c76daba9b0
Zonghao Li <675375410@qq.com>
2018-12-10 18:38:23 +0000

    add more time

--- cypress/integration/books.spec.js
+++ cypress/integration/books.spec.js
@@ -2,7 +2,7 @@ describe('Login and Book operations', () => {
   //need to login first and get the authority to enter books list
   it('should login first ', function () {
     cy.visit('http://localhost:8081/#/login')
-    cy.wait(3000)
+    cy.wait(10000)
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__inner')
       .type('qqq')
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__inner')
@@ -21,7 +21,7 @@ describe('Login and Book operations', () => {
   //delete the new book
   it('delete a book', () => {
     cy.visit('http://localhost:8081/#/books')
-    cy.wait(3000)
+    cy.wait(10000)
     cy.get(':nth-child(10) > :nth-child(2)').should('contain','sun&beach')
     cy.get(':nth-child(10) > :nth-child(3)').should('contain','asdad')
     cy.get(':nth-child(10) > :nth-child(4)').should('contain','qwdhiasdihasd')
