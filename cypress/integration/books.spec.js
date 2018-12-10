72be5c04b9c24114b38ec1d59990a4628e1e7e27
Zonghao Li <675375410@qq.com>
2018-12-10 22:14:29 +0000

    CI - Auto deploy to Surge

--- cypress/integration/books.spec.js
+++ cypress/integration/books.spec.js
@@ -12,32 +12,30 @@ describe('Login and Book operations', () => {
   });
   //add a new book for deleting
   it('should add a book for deleting ', function () {
-    cy.get('[data-test=addbtn] > .nav-link > .fa').click()
-    cy.wait(3000)
-    cy.get('.form-control-label').next().type('sun&beach')
-    // cy.get('[data-test=inputbook]').type()
+    cy.wait(5000)
+    cy.get('[data-test=inputbook]').type('sun&beach')
 
-    cy.get('[data-test=inputauthor]').type('asdad')
+    cy.get('[data-test=inputauthor]').type('lzh')
 
-    cy.get('[data-test=inputsummary]').type('qwdhiasdihasd')
+    cy.get('[data-test=inputsummary]').type('a book')
 
     cy.get('[data-test=summitbtn]').click()
-
+    cy.wait(3000)
   });
   //delete the new book
   it('delete a book', () => {
-    cy.get(':nth-child(1) > :nth-child(2) > .nav-link > .fa').click()
-    cy.wait(3000)
+    cy.visit('http://localhost:8081/#/books')
+    cy.wait(5000)
     cy.get(':nth-child(10) > :nth-child(2)').should('contain','sun&beach')
 
-    cy.get(':nth-child(10) > :nth-child(3)').should('contain','asdad')
+    cy.get(':nth-child(10) > :nth-child(3)').should('contain','lzh')
 
-    cy.get(':nth-child(10) > :nth-child(4)').should('contain','qwdhiasdihasd')
+    cy.get(':nth-child(10) > :nth-child(4)').should('contain','a book')
 
     cy.get(':nth-child(10) > :nth-child(6) > .fa').click()
 
     cy.get('.swal2-confirm').click()
-
+    cy.wait(2000)
     cy.get('.swal2-confirm').click()
   });
 
